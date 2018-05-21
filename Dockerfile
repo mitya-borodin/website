d8c53e89886e7f99aab26e5d7b1b31cecb1e6d38
mitya-borodin <dmitriy@borodin.site>
2018-05-21 09:26:40 +0300

    [IN_PROGRESS] 1. Переход окружения разработки на docker;

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM nginx:1.14.0-alpine
+
+COPY ./dist /usr/share/nginx/html
+
+
+EXPOSE 80:80
---
+++ src/vendors.ts
@@ -0,0 +1,5 @@
+import "isomorphic-style-loader";
+import "normalize.css";
+import "prop-types";
+import "react";
+import "react-dom";
